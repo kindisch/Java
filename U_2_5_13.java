1dcaef2cabc29d4f1412dbc86a14c65bde6edb2c
tibor_hetey <tibor.hetey@expertflow.com>
2016-01-12 16:40:51 +0100

    added 2.5.13

---
+++ U_2_5_13.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+public class U_2_5_13 {
+
+    public static void main(String[] args) {
+      Object [][] zeilenSumme = new Integer [10][10];
+      String [] columnNames = new String [10]; //typumwandlung int zu string notwendig?
+      for(int i = 0;i<10;i++){ //for Schleife, die den ersten Multiplikator(i) hochzählt 
+        for (int g = 0 ;g<10;g++){ //for Schleife, die den zweiten Multiplikator (g) hochzählt 
+          zeilenSumme [i][g] = (i+1)*(g+1);
+        }
+        columnNames[i] = Integer.toString(i+1);
+      }
+      JTable table = new JTable(zeilenSumme, columnNames);
+      JFrame frame = new JFrame("HelloWorldSwing");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.getContentPane().add(table);
+      frame.pack();
+      frame.setVisible(true);
+    }
+}
+
+
